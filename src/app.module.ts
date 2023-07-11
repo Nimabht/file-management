57587ef02bae3d4eba43c9e77237ed4bf75e212d
nimabht <nimabeheshtaein99@gmail.com>
2023-07-11 18:14:18 +0330

    [fix] temp database name fixed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -11,10 +12,11 @@ import { AppService } from './app.service';
       port: 3306,
       username: 'root',
       password: '11NIma22',
-      database: 'file-management',
+      database: 'file_management',
       autoLoadEntities: true,
       synchronize: true,
     }),
+    FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
