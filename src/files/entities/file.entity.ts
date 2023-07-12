719ecd5a3c455f4c7d3e453cdd05412d4ef198c0
nimabht <nimabeheshtaein99@gmail.com>
2023-07-12 17:23:47 +0330

    [refactor] cascade turned true in file entity

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -18,7 +18,9 @@ export class File {
   @Column({ default: 0 })
   remaining_downloads: number;
 
-  @ManyToMany(() => WhitelistedIP)
+  @ManyToMany(() => WhitelistedIP, {
+    cascade: true,
+  })
   @JoinTable({ name: 'file_whiteListIP' })
   whitelistedIPs: WhitelistedIP[];
 }
