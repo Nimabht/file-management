027ffec3bd776d83b444714222a5649b0502f1c7
nimabht <nimabeheshtaein99@gmail.com>
2023-07-12 15:22:12 +0330

    [refactor] config module added globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilesModule } from './files/files.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
@@ -21,6 +26,7 @@ import { MulterModule } from '@nestjs/platform-express';
     MulterModule.register({
       dest: './uploads',
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
